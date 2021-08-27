1dbf0df259718177371642b0015ddcfde43120f6
danielefil <daniele.fil@hotmail.it>
2021-08-27 16:31:58 +0100

    Update

--- ElemFind Dialog/ElemFind_Dialog.py
+++ ElemFind Dialog/ElemFind_Dialog.py
@@ -12,7 +12,7 @@ from FormulaFinder import FormulaFinder, FormulaRefiner
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):   
         super(Ui, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('Dialog_ElementFinder.ui', self) # Load the .ui file
+        uic.loadUi('Dialog_ElementFinder2.ui', self) # Load the .ui file
         self.show()
         # INIT FUNCTIONS
         self.tmpfiles = []
